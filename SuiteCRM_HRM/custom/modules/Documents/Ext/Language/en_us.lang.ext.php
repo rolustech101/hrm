a7b32c7ff576a050f0d059c49365caa3a13de32d
root <root@RT-LT-233.rolustech.net>
2016-12-14 12:49:34 +0500

    .gitignore is now working

--- SuiteCRM_HRM/custom/modules/Documents/Ext/Language/en_us.lang.ext.php
+++
@@ -1,8 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
-$mod_strings['LBL_RT_EMPLOYEES_DOCUMENTS_FROM_RT_EMPLOYEES_TITLE'] = 'Employees';
-$mod_strings['LBL_DOCUMENTS_C'] = 'Employee/Job Application';
-
-?>
