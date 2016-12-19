5bc9cf08bc5f5b288031e44b1608b2286646b61b
Mohsin <mohsin.mumtaz@rolustech.com>
2016-12-19 08:15:55 -0500

    job relate field

---
+++ SuiteCRM_HRM/custom/Extension/modules/RT_Employees/Ext/Vardefs/sugarfield_rt_employees_rt_jobs_name_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2016-12-19 11:05:32
+$dictionary['RT_Employees']['fields']['rt_employees_rt_jobs_name_c']['inline_edit']='1';
+$dictionary['RT_Employees']['fields']['rt_employees_rt_jobs_name_c']['labelValue']='Job Name';
+
+ ?>
---
+++ SuiteCRM_HRM/custom/Extension/modules/RT_Employees/Ext/Vardefs/sugarfield_rt_jobs_id_c.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2016-12-19 11:01:17
+$dictionary['RT_Employees']['fields']['rt_jobs_id_c']['inline_edit']=1;
+
+ ?>
