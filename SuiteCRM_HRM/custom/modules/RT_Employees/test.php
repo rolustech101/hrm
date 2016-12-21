682de21363bbef1f2bedecd4a051901d22687652
Husnain Zaheer <husnain.zaheer@RT-LT-233.rolustech.net>
2016-12-21 11:47:13 +0500

    testing

--- SuiteCRM_HRM/custom/modules/RT_Employees/test.php
+++ SuiteCRM_HRM/custom/modules/RT_Employees/test.php
@@ -1,6 +1,12 @@
 <?php
 echo 'hello';
-//get_tax_calculation('48398866-b758-c521-417b-5853db4af3b5');
-//get_salary_selectlist();
-echo $_SERVER['DOCUMENT_ROOT'];
+global $app_list_strings;
+$list = array();
+if (isset($app_list_strings['countries_dom']))
+{
+    $list = $app_list_strings['countries_dom'];
+    foreach ($list as $key => $value){
+        echo "<option value='$key'>$value<option>";
+    }
+}
 ?>
