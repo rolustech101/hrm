14d324a6a48a15e2bd88f6ea5de76d2546f55313
Husnain Zaheer <husnain.zaheer@RT-LT-233.rolustech.net>
2016-12-23 16:11:02 +0500

    testing

---
+++ SuiteCRM_HRM/custom/Extension/application/Ext/Include/test_hide.php
@@ -0,0 +1,17 @@
+<?php
+
+$modInvisList[] = 'Accounts';
+$modInvisList[] = 'Contacts';
+$modInvisList[] = 'Leads';
+$modInvisList[] = 'Opportunities';
+$modInvisList[] = 'Prospects';
+$modInvisList[] = 'ProspectLists';
+$modInvisList[] = 'Cases';
+$modInvisList[] = 'Project';
+$modInvisList[] = 'Bugs';
+$modInvisList[] = 'Cases';
+$modInvisList[] = 'Tasks';
+$modInvisList[] = 'Meetings';
+$modInvisList[] = 'Calls';
+$modInvisList[] = 'Tasks';
+?>
