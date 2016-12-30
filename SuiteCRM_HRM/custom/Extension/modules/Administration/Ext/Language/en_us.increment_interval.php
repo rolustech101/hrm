115fbe52ea2de91d4402481e27f69b7a732fda70
Husnain Zaheer <husnain.zaheer@RT-LT-233.rolustech.net>
2016-12-30 11:27:15 +0500

    label chnages

--- SuiteCRM_HRM/custom/Extension/modules/Administration/Ext/Language/en_us.increment_interval.php
+++ SuiteCRM_HRM/custom/Extension/modules/Administration/Ext/Language/en_us.increment_interval.php
@@ -1,5 +1,5 @@
 <?php
 
-$mod_strings['LBL_LINK_INCREMENT'] = 'Set Increment Interval/Provident Fund';
-$mod_strings['LBL_INCREMENT_DESCRIPTION'] = 'Set Increment Interval/Provident Fund';
-$mod_strings['LBL_SECTION_HEADER_INCREMENT'] = 'Increment and Provident Funds Settings';
+$mod_strings['LBL_LINK_INCREMENT'] = 'HRM Settings';
+$mod_strings['LBL_INCREMENT_DESCRIPTION'] = 'HRM Settings';
+$mod_strings['LBL_SECTION_HEADER_INCREMENT'] = 'HRM Miscellaneous Settings';
